2d84ea331d8597b9e80e9e798b55ed5c7f714473
engyyezzatt <engyyezzatt@gmail.com>
2023-04-24 23:15:28 +0200

    first commit

---
+++ H-Twonumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double num1, num2; 
+    cin >> num1 >> num2;
+    double result = num1 / num2;
+    cout << "floor " << num1 << " / " << num2 << " = " << floor(result) << endl;
+    cout << "ceil " << num1 << " / " << num2 << " = " << ceil(result) << endl;
+    cout << "round " << num1 << " / " << num2 << " = " << round(result) << endl;
+}
---
+++ J-Multiples.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cin >> num1 >> num2;
+    if (num1 % num2 == 0 || num2 % num1 == 0)
+    {
+        cout << "Multiples" << endl;
+    }
+    else
+    {
+        cout << "No Multiples" << endl;
+    }
+}
